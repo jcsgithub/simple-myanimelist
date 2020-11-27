73797635a055492d29214552ed7a14aee0650a67
Jestoni <jestonisalonga@gmail.com>
2020-11-27 13:11:41 +0800

    Cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import useFetch from "./api/useFetch";
 import AnimeList from "./components/AnimeList";
+import TopBar from "./components/TopBar";
 
 const generateUrl = (type: string, subtype: string) => {
   if (!subtype) {
@@ -22,57 +23,14 @@ const App: React.FC = () => {
     <div className="container">
       <h1>Simple MyAnimeList</h1>
 
-      <div className="top-container">
-        <div className="search-container">
-          <span>Search: </span>
-          <input type="text" placeholder="Search anime ..." />
-        </div>
-
-        <div className="filter-container">
-          <span>Filter by: </span>
-          <select
-            onChange={(e) => {
-              setLoading(true);
-              setType(e.target.value);
-              setSubtype("");
-            }}
-            disabled={loading}
-            name="type"
-            value={type}
-            id="type"
-          >
-            <option value="anime">Anime</option>
-            <option value="manga">Manga</option>
-          </select>
-
-          <select
-            onChange={(e) => {
-              setLoading(true);
-              setSubtype(e.target.value);
-            }}
-            disabled={loading}
-            className={type === "manga" ? "hidden" : ""}
-            name="subtype"
-            value={subtype}
-            id="subtype"
-          >
-            <option value="">Select one...</option>
-            <option value="airing">Airing</option>
-            <option value="upcoming">Upcoming</option>
-            <option value="movie">Movie</option>
-          </select>
-        </div>
-
-        <div className="sort-container">
-          <span>Sort by: </span>
-          <select name="sort" defaultValue="" id="sort">
-            <option value="">Select one...</option>
-            <option value="airing">Popularity</option>
-            <option value="upcoming">Name</option>
-            <option value="movie">Aired date</option>
-          </select>
-        </div>
-      </div>
+      <TopBar
+        type={type}
+        subtype={subtype}
+        loading={loading}
+        setType={setType}
+        setSubtype={setSubtype}
+        setLoading={setLoading}
+      />
 
       <AnimeList data={data} loading={loading} />
     </div>
---
+++ src/components/TopBar.tsx
@@ -0,0 +1,66 @@
+import React from "react";
+
+interface TopBarProps {
+  type: string;
+  subtype: string;
+  loading: boolean;
+  setType: React.Dispatch<React.SetStateAction<string>>;
+  setSubtype: React.Dispatch<React.SetStateAction<string>>;
+  setLoading: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+export const TopBar: React.FC<TopBarProps> = (app: TopBarProps) => {
+  return (
+    <div className="top-container">
+      <div className="filter-container">
+        <span>Filter by: </span>
+        <select
+          onChange={(e) => {
+            app.setLoading(true);
+            app.setType(e.target.value);
+            app.setSubtype("");
+          }}
+          disabled={app.loading}
+          name="type"
+          value={app.type}
+          id="type"
+        >
+          <option value="anime">Anime</option>
+          <option value="manga">Manga</option>
+        </select>
+
+        <select
+          onChange={(e) => {
+            app.setLoading(true);
+            app.setSubtype(e.target.value);
+          }}
+          disabled={app.loading}
+          className={app.type === "manga" ? "hidden" : ""}
+          name="subtype"
+          value={app.subtype}
+          id="subtype"
+        >
+          <option value="">Select one...</option>
+          <option value="airing">Airing</option>
+          <option value="upcoming">Upcoming</option>
+          <option value="movie">Movie</option>
+        </select>
+
+        <span>Sort by: </span>
+        <select name="sort" defaultValue="" id="sort">
+          <option value="">Select one...</option>
+          <option value="airing">Popularity</option>
+          <option value="upcoming">Name</option>
+          <option value="movie">Aired date</option>
+        </select>
+      </div>
+
+      <div className="search-container">
+        <span>Search: </span>
+        <input type="text" placeholder="Search anime ..." />
+      </div>
+    </div>
+  );
+};
+
+export default TopBar;
